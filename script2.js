4929af6c8510ba592da84583fb6994dcc3ad699b
Kyle Cash <kcashdev@gmail.com>
2021-03-17 20:06:07 +0700

    uploading files

---
+++ script2.js
@@ -0,0 +1,584 @@
+let methods = {
+
+}
+
+
+
+
+
+let obj = {
+    acos: {
+        acos: {
+        name: 'AC',
+        description: 'Math.acos()'
+        }
+    },
+    asin:{
+        asin: {
+        name: 'AS',
+        description: 'Math.asin()'
+        }
+    },
+    ceil: {
+        name: 'CE',
+        description: 'Math.ceil()'
+    },
+    cos: {
+        name: 'CS',
+        description: 'Math.cos()'
+    },
+    floor: {
+        name: 'FL',
+        description: 'Math.floor()'
+    },
+    min: {
+        name: 'MN',
+        description: 'Math.min()'
+    },
+    max: {
+        name: 'MX',
+        description: 'Math.max()'
+    },
+    pow: {
+        name: 'PW',
+        description: 'Math.pow()'
+    },
+    random: {
+        name: 'RM',
+        description: 'Math.random()'
+    },
+    round: {
+        name: 'RO',
+        description: 'Math.round()'
+    },
+    sin: {
+        name: 'SI',
+        description: 'Math.sin()'
+    },
+    sqrt: {
+        name: 'SQ',
+        description: 'Math.sqrt()'
+    },
+    trunc: {
+        name: 'TR',
+        description: 'Math.trunc()'
+    },
+    array: {
+        concat: {
+            name: 'CO',
+            description: '.concat()'
+        },
+        indexOf: {
+            name: 'IO',
+            description: '.indexOf()'
+        },
+        isArray: {
+            name: 'IA',
+            description: '.isArray()'
+        },
+        filter: {
+            name: 'FI',
+            description: '.filter()'
+        },
+        forEach: {
+            name: 'FE',
+            description: '.forEach()'
+        },
+        join: {
+            name: 'JO',
+            description: '.join()'
+        },
+        lastIndexOf: {
+            name: 'LI',
+            description: '.lastIndexOf()'
+        },
+        map: {
+            name: 'MP',
+            description: '.map()'
+        },
+        pop: {
+            name: 'PO',
+            description: '.pop()'
+        },
+        push: {
+            name: 'PU',
+            description: '.push()'
+        },
+        reduce: {
+            name: 'RD',
+            description: '.reduce()'
+        },
+        reverse: {
+            name: 'RV',
+            description: '.reverse()'
+        },
+        shift: {
+            name: 'SH',
+            description: '.shift()'
+        },
+        slice: {
+            name: 'SL',
+            description: '.slice()'
+        },
+        splice: {
+            name: 'SP',
+            description: '.splice()'
+        },
+        toString: {
+            name: 'TS',
+            description: '.toString()'
+        },
+        unshift: {
+            name: 'US',
+            description: '.unshift()'
+        },
+        valueOf: {
+            name: 'VO',
+            description: '.valueOf()'
+        }
+    },
+    string: {
+        charAt: {
+            name: 'CA',
+            description: '.charAt()'
+        },
+        charCodeAt: {
+            name: 'CC',
+            description: '.charCodeAt()'
+        },
+        endsWith: {
+            name: 'EW',
+            description: '.endsWith()'
+        },
+        includes: {
+            name: 'IN',
+            description: '.includes()'
+        },
+        fromCharCode: {
+            name: 'FC',
+            description: '.fromCharCode()'
+        },
+        localeCompare: {
+            name: 'LC',
+            description: '.localeCompare()'
+        },
+        match: {
+            name: 'MA',
+            description: '.match()'
+        },
+        padStart: {
+            name: 'PS',
+            description: '.padStart()'
+        },
+        padEnd: {
+            name: 'PE',
+            description: '.padEnd()'
+        },
+        repeat: {
+            name: 'RT',
+            description: '.repeat()'
+        },
+        replace: {
+            name: 'RP',
+            description: '.replace()'
+        },
+        search: {
+            name: 'SE',
+            description: '.search()'
+        },
+        split: {
+            name: 'ST',
+            description: '.split()'
+        },
+        substr: {
+            name: 'SB',
+            description: '.substr()'
+        },
+        substring: {
+            name: 'SS',
+            description: '.substring()'
+        },
+        startsWith: {
+            name: 'SW',
+            description: '.startsWith()'
+        },
+        toLowerCase: {
+            name: 'TL',
+            description: '.toLowerCase()'
+        },
+        toUpperCase: {
+            name: 'TU',
+            description: '.toUpperCase()'
+        },
+        trim: {
+            name: 'TR',
+            description: '.trim()'
+        },
+        toString: {
+            name: 'TS',
+            description: '.toString()'
+        },
+        valueOf: {
+            name: 'VO',
+            description: '.valueOf()'
+        },
+        htmlWrappers: {
+            name: 'HT',
+            description: 'See more'
+        }
+    },
+    global: {
+        decodeURI: {
+            name: 'DU',
+            description: '.decodeURI()'
+        },
+        decodeURIComponent: {
+            name: 'DC',
+            description: '.decodeURIComponent()'
+        },
+        encodeURI: {
+            name: 'EU',
+            description: '.encodeURI()'
+        },
+        encodeURIComponent: {
+            name: 'EC',
+            descriptions: '.encodeURIComponent()'
+        },
+        eval: {
+            name: 'EV',
+            description: '.eval()'
+        },
+        isFinite: {
+            name: 'IF',
+            description: '.isFinite()'
+        },
+        isNaN: {
+            name: 'IN',
+            description: '.isNaN()'
+        },
+        number: {
+            name: 'NU',
+            description: 'Number()'
+        },
+        parseFloat: {
+            name: 'PF',
+            description: 'parseFloat()'
+        },
+        parseInt: {
+            name: 'PI',
+            description: 'parseInt()'
+        },
+        String: {
+            name: 'ST',
+            description: 'String()'
+        }
+    },
+    window: {
+        alert: {
+            name: 'AL',
+            description: 'alert()'
+        },
+        blur: {
+            name: 'BL',
+            description: 'blur()'
+        },
+        clearInterval: {
+            name: 'CI',
+            description: 'clearInterval()'
+        },
+        clearTimeout: {
+            name: 'CT',
+            descriptions: 'clearTimeout()'
+        },
+        close: {
+            name: 'CL',
+            description: 'close()'
+        },
+        confirm: {
+            name: 'CF',
+            description: 'confirm()'
+        },
+        find: {
+            name: 'FI',
+            description: '.find()'
+        },
+        focus: {
+            name: 'FO',
+            description: 'focus()'
+        },
+        moveBy: {
+            name: 'MB',
+            description: 'moveBy()'
+        },
+        moveTo: {
+            name: 'MT',
+            description: 'moveTo()'
+        },
+        open: {
+            name: 'OP',
+            description: 'open()'
+        },
+        print: {
+            name: 'PR',
+            description: 'print()'
+        },
+        prompt: {
+            name: 'PM',
+            description: 'prompt()'
+        },
+        resizeBy: {
+            name: 'RB',
+            description: 'resizeBy()'
+        },
+        resizeTo: {
+            name: 'RT',
+            description: 'resizeTo()'
+        },
+        scrollBy: {
+            name: 'SB',
+            description: 'scrollBy()'
+        },
+        scrollTo: {
+            name: 'ST',
+            description: 'scrollTo()'
+        },
+        setInterval: {
+            name: 'SI',
+            description: 'setInterval()'
+        },
+        setTimeout: {
+            name: 'ST',
+            description: 'setTimeout()'
+        },
+        stop: {
+            name: 'SP',
+            description: 'stop()'
+        }
+    },
+    element: {
+        getAttribute: {
+            name: 'GA',
+            description: '.getAttribute()'
+        },
+        getAttributeNS: {
+            name: 'GN',
+            description: '.getAttributeNS()'
+        },
+        getAttributeNode: {
+            name: 'AN',
+            description: '.getAttributeNode()'
+        },
+        getAttributeNodeNS: {
+            name: 'NN',
+            descriptions: '.getAttributeNodeNS()'
+        },
+        getElementsByTagName: {
+            name: 'GT',
+            description: '.getElementsByTagName()'
+        },
+        getElementsByTagNameNS: {
+            name: 'TN',
+            description: '.getElementsByTagNameNS()'
+        },
+        hasAttribute: {
+            name: 'HA',
+            description: '.hasAttribute()'
+        },
+        hasAttributeNS: {
+            name: 'HN',
+            description: '.hasAttributeNS()'
+        },
+        removeAttribute: {
+            name: 'RA',
+            description: '.removeAttribute()'
+        },
+        removeAttributeNS: {
+            name: 'RN',
+            description: '.removeAttributeNS()'
+        },
+        removeAttributeNode: {
+            name: 'AT',
+            description: '.removeAttributeNode()'
+        },
+        setAttribute: {
+            name: 'SA',
+            description: '.setAttribute()'
+        },
+        setAttributeNS: {
+            name: 'SS',
+            description: '.setAttributeNS()'
+        },
+        setAttributeNode: {
+            name: 'SN',
+            description: '.setAttributeNode()'
+        },
+        setAttributeNodeNS: {
+            name: 'AS',
+            description: '.setAttributeNodeNS()'
+        }
+    },
+    node: {
+        appendChild: {
+            name: 'AC',
+            description: '.appendChild()'
+        },
+        cloneNode: {
+            name: 'CN',
+            description: '.cloneNode()'
+        },
+        compareDocumentPosition: {
+            name: 'CP',
+            description: '.compareDocumentPosition()'
+        },
+        getFeature: {
+            name: 'GF',
+            descriptions: '.getFeature()'
+        },
+        hasAttributes: {
+            name: 'HT',
+            description: '.hasAttributes()'
+        },
+        hasChildNodes: {
+            name: 'HC',
+            description: '.hasChildNodes()'
+        },
+        insertBefore: {
+            name: 'IB',
+            description: '.insertBefore()'
+        },
+        isDefaultNamespace: {
+            name: 'ID',
+            description: '.isDefaultNamespace()'
+        },
+        isEqualNode: {
+            name: 'IE',
+            description: '.isEqualNode()'
+        },
+        isSameNode: {
+            name: 'IS',
+            description: '.isSameNode()'
+        },
+        lookupNamespaceURI: {
+            name: 'LN',
+            description: '.lookupNamespaceURI()'
+        },
+        lookupPrefix: {
+            name: 'LP',
+            description: '.lookupPrefix()'
+        },
+        normalize: {
+            name: 'NO',
+            description: '.normalize()'
+        },
+        removeChild: {
+            name: 'RM',
+            description: '.removeChild()'
+        },
+        replaceChild: {
+            name: 'RC',
+            description: '.replaceChild()'
+        }
+    },
+    number: {
+        number: {
+            name: 'NU',
+            description: 'Number()'
+        },
+        parseInt : {
+            name: 'PI',
+            description: 'parseInt()'
+        },
+        toExponential: {
+            name: 'TE',
+            description: '.toExponential()'
+        },
+        toFixed: {
+            name: 'TF',
+            description: '.toFixed()'
+        },
+        toPrecision: {
+            name: 'TP',
+            description: '.toPrecision()'
+        },
+        toString: {
+            name: 'TS',
+            descriptions: '.toString()'
+        },
+        valueOf: {
+            name: 'VO',
+            description: '.valueOf()'
+        }
+    }
+};
+
+const table = document.querySelector('#periodic-table');
+table.addEventListener('click', (e) => {
+    const activeDiv = document.querySelector('.active');
+    let target = e.target;
+    if (target.classList.contains('element')) {
+        if (!activeDiv) {
+            target.classList.toggle('active');
+        } else if (activeDiv === target) {
+            activeDiv.classList.toggle('active');
+        } else {
+            console.log(activeDiv);
+            activeDiv.classList.toggle('active');
+            target.classList.add('active');
+        }
+    }
+});
+
+const mathBox = document.querySelector('.mathBox');
+const math = document.querySelectorAll('.math');
+const numberBox = document.querySelector('.numberBox');
+const number = document.querySelectorAll('.number');
+const arrayBox = document.querySelector('.arrayBox');
+const array = document.querySelectorAll('.array');
+const stringBox = document.querySelector('.stringBox');
+const string = document.querySelectorAll('.string');
+const elementsBox = document.querySelector('.elementsBox');
+const elements = document.querySelectorAll('.elements');
+const nodeBox = document.querySelector('.nodeBox');
+const node = document.querySelectorAll('.node');
+const globalBox = document.querySelector('.globalBox');
+const global = document.querySelectorAll('.global');
+const windowBox = document.querySelector('.windowBox');
+const windows = document.querySelectorAll('.window');
+
+
+
+function identifyMethods(box, methodArea) {
+    box.addEventListener('mouseover', () => {
+        methodArea.forEach(el => {
+            el.style.boxShadow = '.3rem .3rem .8rem white';
+            el.style.transform = 'translateY(-3px)';
+        });
+    })
+};
+
+function unidentifyMethods(box, methodArea) {
+    box.addEventListener('mouseout', () => {
+        methodArea.forEach(el => {
+            el.style.boxShadow = '';
+            el.style.transform = 'translateY(0)';
+        });
+    })
+}
+
+identifyMethods(mathBox, math);
+unidentifyMethods(mathBox, math);
+identifyMethods(numberBox, number);
+unidentifyMethods(numberBox, number);
+identifyMethods(elementsBox, elements);
+unidentifyMethods(elementsBox, elements);
+identifyMethods(nodeBox, node);
+unidentifyMethods(nodeBox, node);
+identifyMethods(globalBox, global);
+unidentifyMethods(globalBox, global);
+identifyMethods(windowBox, windows);
+unidentifyMethods(windowBox, windows);
+identifyMethods(stringBox, string);
+unidentifyMethods(stringBox, string);
+identifyMethods(arrayBox, array);
+unidentifyMethods(arrayBox, array);
+
